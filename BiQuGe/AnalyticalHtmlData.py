a438cd78c93ccd00526b56c26fa86bffe4efd5dc
chenhuan <chenhuan@bonbonwork.com>
2019-03-06 17:30:33 +0800

    okokoko

--- BiQuGe/AnalyticalHtmlData.py
+++ BiQuGe/AnalyticalHtmlData.py
@@ -9,21 +9,24 @@ header = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image
 "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
 "Cache-Control": "max-age=0",
 "Connection": "keep-alive",
-"Cookie":"__cfduid=dde15d9931ebc00af12e2f79cc87a663b1551713464; UM_distinctid=1694954c8ee19d-04a1c9a712605f-36607102-fa000-1694954c8ef23c; CNZZDATA1271465101=1834752202-1551709448-null%7C1551709448",
+"Cookie":"__cfduid=dc333734c30600ce7da3b412dce7e88101551851831; UM_distinctid=1695193cf353e3-00fb4c3e875422-36607102-1fa400-1695193cf36723; CNZZDATA1271465101=402203686-1551851591-%7C1551851591",
 "Host": "www.biquge.cm",
 "Referer": "http://www.biquge.cm/paihangbang/",
 "Upgrade-Insecure-Requests": "1",
 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"}
 htmlText = requests.get(url, headers = header);
 htmlText.encoding = "gbk";
-bs = BeautifulSoup(htmlText.text,"lxml").find("div",class_="box b4").find_all('a');
-for a in bs:
-    a_url = a['href']
-    a_title = a.string
+print(htmlText.text)
+bs = BeautifulSoup(htmlText.text,"lxml").find("div",id="main").find_all('li')
+for aa in bs:
+    print(aa.find('a'))
+    if aa.find('a') != None:
+        a_url = aa.find('a')['href']
+        a_title = aa.find('a').string
+        print(a_url)
+        print(a_title)
 
-#a_title = a.find('a').get("title")
-    print(a_url)
-    print(a_title)
-    sql = "INSERT INTO pystroy (storyurl,storyname,status) VALUES (%s,%s,%s)"
-    data = (a_url, a_title, 1)
-    db.cud(sql, data)
+
+        sql = "INSERT INTO pystroy (storyurl,storyname,status) VALUES (%s,%s,%s)"
+        data = (a_url, a_title, 1)
+        db.cud(sql, data)
